97074ea68cf058d368ae57515186a6c6f74ab309
SaiVarshithM <117940405+SaiVarshithBeginner@users.noreply.github.com>
2024-02-01 22:22:30 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import Navbar from './components/Navbar';
+
+
+
+function App() {
+  return (
+   <>
+  
+<Navbar title="TEXTUTILS" aboutText="About Textutils"/>
+   
+   </>
+    
+  );
+}
+
+export default App;
